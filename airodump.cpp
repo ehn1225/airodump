c790afeea97b5f378ad49c2734765c089ee1adbe
ehn1225 <ehn1225@naver.com>
2023-01-06 02:08:12 -0500

    [airodump][이예찬] frequency, type, ch 구현

--- airodump.cpp
+++ airodump.cpp
@@ -5,14 +5,20 @@
 #include <iostream>
 #include <string.h>
 #include "mac.h"
+#include <map>
 
 using namespace std;
 
 struct ieee80211_radiotap_header {
-    u_char it_version;     /* set to 0 */
+    u_char it_version;
     u_char it_pad;
-    u_int16_t it_len;         /* entire length */
-	u_char it_present[4];     /* fields present */
+    u_int16_t it_len;
+	u_char it_present_flags[12];
+	u_char flags;
+	u_char dataRate;
+	u_int16_t ch_frequency;
+	u_int16_t ch_flags;
+	u_char antenna_signal;	
 };
 
 struct ieee80211_beacon_frame{
@@ -21,6 +27,15 @@ struct ieee80211_beacon_frame{
 	Mac dstAddr;
 	Mac srcArrr;
 	Mac bssId;
+};
+
+struct ieee80211_wireless_management{
+	u_int64_t timestamp;
+	u_int16_t beaconInterval;
+	u_int16_t capablityInfo;
+	u_int8_t tagNumber;
+	u_int8_t tagLength;
+	unsigned char SSID[33];
 };
 
 void usage() {
@@ -55,6 +70,7 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
 		return -1;
 	}
+
 	while (true) {
 		struct pcap_pkthdr* header;
 		const unsigned char* packet;
@@ -68,6 +84,7 @@ int main(int argc, char* argv[]) {
 		//802.11 Radiotap에서 Radiotap 해더의 길이를 가져오고, 이를 통해 다음 레이어로 넘어감
 		struct ieee80211_radiotap_header *radiotap = (struct ieee80211_radiotap_header *)packet;
 		struct ieee80211_beacon_frame *beaconframe = (struct ieee80211_beacon_frame *)(packet + radiotap->it_len);
+		struct ieee80211_wireless_management *wireless_mgr = (struct ieee80211_wireless_management *)(packet + radiotap->it_len + 24);
 
 		//Beacon 프레임인지 Type 확인
 		if(ntohs(beaconframe->frameCtl) != 0x8000){
@@ -75,46 +92,19 @@ int main(int argc, char* argv[]) {
 		    continue; 
 		}
 
-		printf("SA : %s, BSSID : %s\n", string(beaconframe->srcArrr).c_str(), string(beaconframe->bssId).c_str());
-		//cout << string(beaconframe->dstAddr) << endl;
-		// struct ip *ip_hdr = (struct ip *)(packet+SIZE_ETHERNET); //IP Header Struct
-		// //if next layer is not tcp, move next packet
-		// if(ip_hdr->ip_p != 0x06){
-		// 	printf("This Packet is not TCP  ptotocol\n");
-		//        	continue;
-		// }
-		// u_int size_ip;
-		// u_int size_tcp;
-		// size_ip = IP_HL(ip_hdr)*4;
-		// struct tcp *tcp_hdr = (struct tcp*)(packet + SIZE_ETHERNET + size_ip); //TCP Header Struct
-		// size_tcp = TH_OFF(tcp_hdr)*4; 
-		
-		// printf("================ Ethernet ================\n");
-		// printf("Source MAC Address     : %02x:%02x:%02x:%02x:%02x:%02x\n", eth_hdr->ether_shost[0],eth_hdr->ether_shost[1],eth_hdr->ether_shost[2],eth_hdr->ether_shost[3],eth_hdr->ether_shost[4],eth_hdr->ether_shost[5]);
-		// printf("Dstination MAC Address : %02x:%02x:%02x:%02x:%02x:%02x\n", eth_hdr->ether_dhost[0],eth_hdr->ether_dhost[1],eth_hdr->ether_dhost[2],eth_hdr->ether_dhost[3],eth_hdr->ether_dhost[4],eth_hdr->ether_dhost[5]);
-
-		// printf("=================== IP ===================\n");
-		// printf("Source IP Address     : %s\n", inet_ntoa(ip_hdr->ip_src));
-		// printf("Destnation IP Address : %s\n", inet_ntoa(ip_hdr->ip_dst));
-
-		// printf("================== TCP ===================\n");
-        // 	printf("Source Port     : %d\n", ntohs(tcp_hdr->th_sport));
-        // 	printf("Destnation Port : %d\n", ntohs(tcp_hdr->th_dport));
-		// const u_char * payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
-		// u_int payload_size = ntohs(ip_hdr->ip_len) - size_ip - size_tcp;
-		// if(payload_size > 10){payload_size = 10;}
-		// if(payload_size == 0){
-		// 	printf("No Data");
-		// }
-		// else{
-		// 	for(int i =0; i < payload_size;i++){
-		// 		printf("%02x ", payload[i]);
-		// 	}	
-		
-		
-		// }
-		// printf("\n============= End Of Packet ==============\n\n\n");
-
+		if(wireless_mgr->tagNumber != 0){
+			printf("tagNumber 0\n");
+		    continue; 
+		}
+		else{
+			bool twodotfour = (radiotap->ch_flags & 0x0080);
+			int ch = -1;
+			if(twodotfour){
+				ch = ((radiotap->ch_frequency - 2412) / 5) + 1;
+			}
+			wireless_mgr->SSID[wireless_mgr->tagLength] = '\0';
+			printf("BSSID : %s, SSID : %s, frequency : %dMhz, type : %s, ch : %d\n", string(beaconframe->srcArrr).c_str(), wireless_mgr->SSID, radiotap->ch_frequency, (twodotfour) ? "2.4GHz" : "5GHz", ch);
+		}
 			
 	}
 
